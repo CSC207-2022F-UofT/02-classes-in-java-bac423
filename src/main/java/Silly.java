04b261bca47e69d1c658bfe50d206dc1e9ececd8
Alexander Ryabchenko <alexander.ryabchenko@mail.utoronto.ca>
2022-09-20 17:19:06 -0400

    second commit

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -159,7 +159,6 @@ public class Silly implements Comparable<Silly>{
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
-
         return this.name.equals(other.name);
     }
 
